399de4640f22de692cddc2bf8e993e0bd0d62988
hw1964 <hw1964@nyu.edu>
2019-11-25 21:27:43 -0500

    better version

--- task1/test.sh
+++ task1/test.sh
@@ -1,26 +1,24 @@
-#!/bim/bash
+#!/bin/bash
 module load python/gnu/3.6.5
-module load spark/2.4.4 
+module load spark/2.4.0 
 
-hdfs dfs -get /user/hm74/NYCOpenData/datasets.tsv
-list=$(hdfs dfs -ls /user/hm74/NYCOpenData/*.gz)
-i=0
-for item in $list; do
-        verify=$(echo $item|grep .tsv.gz)
-        if [ $verify ]; then
-                i=$((i+1))
-                if [ $i -gt 951 ]; then
-                        break;
-                fi
+min=$1
+max=$2
+if [ -z ${min} ];then
+exit 0
+fi
+if [ -z ${max} ];then
+exit 0
+fi
+input="dataset_index.txt"
+while IFS= read -r line
+do
+	index=$(echo $line | cut -d "," -f1) # the index
+	fileName=$(echo $line | cut -d "," -f2) # Name of the file
+	datasetName=$(echo $line | cut -d "," -f3) # Name of the dataset
+	if [ $index -ge $min ] && [ $index -lt $max ]; then
+		spark-submit test.py $fileName $datasetName
+	fi
+done < "$input"
 
-                echo " this is the $i file"
-                # verify is the address of the public dataset
-                echo $verify
-                # needs to chagne from /user/hm74/NYCOpenData/ywiv-5gyw.tsv.gz to ywiv-5gyw
-                foo=${verify%".tsv.gz"}
-                foo=$(echo $foo | sed 's!/user/hm74/NYCOpenData/!!')
-                ds_name=$(grep $foo datasets.tsv)
-                ds_result=$(echo ${ds_name//$foo/})
-                spark-submit test.py "$verify" "$ds_result"
-        fi
-done
+echo "all jobs started, check ps for background process display"
