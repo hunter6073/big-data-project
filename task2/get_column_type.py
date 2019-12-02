e5c8ed4b666a360656bac39be6f1a3008e22a384
hunter6073 <hw1964@nyu.edu>
2019-12-02 16:16:28 -0500

    Update get_column_type.py

--- task2/get_column_type.py
+++ task2/get_column_type.py
@@ -20,11 +20,40 @@ for i in range(0,len(onlyfiles)):
     key_list = []
     while line:
         l_key = line.split(",")[0].replace("'","").strip()
+        if l_key == "Business Name":
+            l_key = "Business name"
+        if "name" in l_key:
+            l_key = "Person name"
+        if l_key == "Letter":
+            l_key = "Other"
+        if l_key == "Park":
+            l_key = "Parks/Playgrounds"
+        if l_key == "Phone number":
+            l_key = "Phone Number"
+        if l_key == "School levels":
+            l_key = "School Levels"
+        if l_key == "other":
+            l_key = "Other"
+
         if l_key not in key_list:
             key_list.append(l_key) # add to the key_list array
         line = fp.readline()
 
     for kl_key in key_list: # add keys to data dictionary
+        if kl_key == "Business Name":
+            kl_key = "Business name"
+        if "name" in kl_key:
+            kl_key = "Person name"
+        if kl_key == "Letter":
+            kl_key = "Other"
+        if kl_key == "Park":
+            kl_key = "Parks/Playgrounds"
+        if kl_key == "Phone number":
+            kl_key = "Phone Number"
+        if kl_key == "School levels":
+            kl_key = "School Levels"
+        if kl_key == "other":
+            kl_key = "Other"
         if kl_key in data.keys():
             data[kl_key] += 1
         else:
